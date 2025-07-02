9a19e40f30d7a5212b40996fab4426e7ea6ad326
tahiana1 <tahianaflavienabia@gmail.com>
2025-07-03 00:50:18 +0300

    updated:note

--- frontend/src/src/routes.ts
+++ frontend/src/src/routes.ts
@@ -6,7 +6,7 @@ export const ROUTES = {
   deposit: "/billing/deposit",
   profile: "/profile",
   point: "/profile/point",
-  inbox: "/profile/note",
+  inbox: "/note",
   attends: "/profile/attends",
   roulette: "/profile/roulette",
 
