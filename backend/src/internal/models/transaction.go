2d589dd1f3feb272f6fbdb97b809b460ca25880f
tahiana1 <tahianaflavienabia@gmail.com>
2025-07-15 06:12:48 +0300

    updated

--- backend/src/internal/models/transaction.go
+++ backend/src/internal/models/transaction.go
@@ -12,17 +12,17 @@ type Transaction struct {
 	UserID uint `json:"userId"`
 	User   User `gorm:"foreignKey:UserID"`
 
-	Type   string  `json:"type" gorm:"size:20"`    // "deposit" or "withdrawal"
+	Type   string  `json:"type" gorm:"size:200"`   // "deposit" or "withdrawal"
 	Amount float64 `json:"amount" gorm:"not null"` // Amount of the transaction
 
-	BalanceBefore float64 `json:"balanceBefore"` // User's balance before the transaction
-	BalanceAfter  float64 `json:"balanceAfter"`  // User's balance after the transaction
-	Explation     string  `json:"explation"`     // User's explation
-	PointBefore   float64 `json:"pointBefore"`   // User's point before the transaction
-	PointAfter    float64 `json:"pointAfter"`    // User's point after the transaction
+	BalanceBefore float64 `json:"balanceBefore"`             // User's balance before the transaction
+	BalanceAfter  float64 `json:"balanceAfter"`              // User's balance after the transaction
+	Explation     string  `json:"explation" gorm:"size:100"` // User's explation
+	PointBefore   float64 `json:"pointBefore"`               // User's point before the transaction
+	PointAfter    float64 `json:"pointAfter"`                // User's point after the transaction
 
-	Status   string `json:"status" gorm:"size:20"`    // e.g., "pending", "completed", "failed"
-	Shortcut string `json:"shortcut" gorm:"size:20"`  // e.g., "pending", "completed", "failed"
+	Status   string `json:"status" gorm:"size:200"`   // e.g., "pending", "completed", "failed"
+	Shortcut string `json:"shortcut" gorm:"size:200"` // e.g., "pending", "completed", "failed"
 	USDTDesc string `json:"usdtDesc" gorm:"size:200"` // e.g., "pending", "completed", "failed"
 
 	TransactionAt time.Time `json:"transactionAt" gorm:"autoCreateTime"`
